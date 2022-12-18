c30fea4a462292d2624698b0e29adfc67a306f06
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-12-18 00:59:21 -0500

    use of interfaces

--- src/index.ts
+++ src/index.ts
@@ -72,3 +72,20 @@ function addNum(x:number,y:number):number{
 function log(message:string|number):void{
     console.log(message)
 }
+
+//Interfaces
+interface UserInterface{
+    readonly id: number, // cannot assign it again because it is readonly property
+    name: string,
+    age?: number // "?" shows that the field is optional
+}
+const user1: UserInterface ={
+    id:1,
+    name: "John"
+}
+
+interface MathFunc{
+    (x:number,y:number):number
+}
+const add:MathFunc = (x:number,y:number):number=> x + y
+const sub:MathFunc = (x:number,y:number):number=> x - y
